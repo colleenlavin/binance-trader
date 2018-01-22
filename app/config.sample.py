bc5c8328378bccba343747b2ec23da549c326277
Colleen Lavin <cml388@mun.ca>
2018-01-22 12:46:20 -0600

    changed one line

---
+++ app/config.sample.py
@@ -0,0 +1,9 @@
+# @yasinkuyu
+
+# Get an Key and Secret 
+# https://www.kucoin.com/#/user/setting/api
+
+api_key = 'WBBcJ2miX5E9etn4Y9PxtujEoe8BDb4r3bPvwuoU7zkrFzMGPXvogeGPy7mLJ2vR'
+api_secret = '8boaOQWfqBr8ptjpFTurdJUwswyGcqRmP2UUOUb98Q8Uw5Is7bh363O0zpaZ0tA1'
+
+recv_window = 6000000
